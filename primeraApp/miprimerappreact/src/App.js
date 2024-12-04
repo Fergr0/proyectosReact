6b76a735a82166d890f1e563b043eea7b4d15dc7
Fergr0 <fergr01bbx@gmail.com>
2024-12-04 12:28:55 +0100

    funcion flecha para controlar eventos

--- primeraApp/miprimerappreact/src/App.js
+++ primeraApp/miprimerappreact/src/App.js
@@ -4,12 +4,27 @@ import HelloComponent from './components/HelloComponent';
 import { Component } from 'react';
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state={
+      name: 'Fernando'
+    }
+  }
+
+  changeName=(event)=>{
+    this.setState({
+      name: event.target.value
+    })
+  }
+
   render(){
     return(
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo'></img>
-          <HelloComponent></HelloComponent>
+          <HelloComponent nombre = {this.state.name}></HelloComponent>
+          <input type='text' value={this.state.name} onChange={this.changeName}></input>
         </header>
       </div>
     );
