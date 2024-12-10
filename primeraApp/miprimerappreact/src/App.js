af12cdd119ab222e75725db9c7df992495d0ba7c
Fergr0 <fergr01bbx@gmail.com>
2024-12-10 09:35:39 +0100

    callback entre inputcomponet y app.js

--- primeraApp/miprimerappreact/src/App.js
+++ primeraApp/miprimerappreact/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import HelloComponent from './components/HelloComponent';
 import { Component } from 'react';
-import InputComponent from './components/Imputomponet';
 
 class App extends Component {
 
