199f8ada4db1018134ae0e9a67b4df6cc3e36b22
Fernando <119356551+Fergr0@users.noreply.github.com>
2024-12-17 12:44:18 +0100

    Update DetailsWrapper.jsx

--- proyecto-pokemon/pokemon-project/src/hoc/DetailsWrapper.jsx
+++ proyecto-pokemon/pokemon-project/src/hoc/DetailsWrapper.jsx
@@ -1,19 +1,13 @@
 import { useState } from "react";
 
-function DetailsWrapper(DetailsComponent){
-    function newComponent(props){
-        const [likes, setLikes] = useState(0);
-        const increaseLikes = () =>{
-            setLikes(likes+1);
-        }
-        return (
-            <DetailsComponent pokemon = {props.pokemon} likes = {likes} increaseLikes = {increaseLikes}></DetailsComponent>
-        )
+function DetailsWrapper(props){
+
+    const [likes, setLikes] = useState(0);
+    const increaseLikes = () =>{
+        setLikes(likes+1);
     }
 
-
-
-    return newComponent;
+    return <>{props.render(likes, increaseLikes)}</>;
 
 }
 
