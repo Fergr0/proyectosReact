5e7d9b36a794835b4c76af13143688a8348e8ac6
Fernando <119356551+Fergr0@users.noreply.github.com>
2024-12-17 12:45:15 +0100

    Update App.jsx

--- proyecto-pokemon/pokemon-project/src/App.jsx
+++ proyecto-pokemon/pokemon-project/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import PokemonListComponent from './components/PokemonListComponent'
 import PokemonDetailsComponent from './components/PokemonDetailsComponent'
 import PokemonDetailsComponent2 from './components/PokemonDetailsComponent2'
+import DetailsWrapper from './hoc/DetailsWrapper'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,20 +13,26 @@ function App() {
   const [selectedPokemon, setSelectedPokemon] = useState()
   const [selectedPokemon2, setSelectedPokemon2] = useState()
 
+  const getDetails1 = (likes, increaseLikes) => {
+    return(
+      <PokemonDetailsComponent pokemon={selectedPokemon} likes = {likes} increaseLikes = {increaseLikes}></PokemonDetailsComponent>
+    )
+  }
+
+  const getDetails2 = (likes, increaseLikes) => {
+    return(
+      <PokemonDetailsComponent2 pokemon={selectedPokemon2} likes = {likes} increaseLikes = {increaseLikes}></PokemonDetailsComponent2>
+    )
+  }
+
   return (
     <>
     <h2>Pokémons seleccionados</h2>
       {selectedPokemon && (
-        <div>
-          
-          <PokemonDetailsComponent pokemon = {selectedPokemon}></PokemonDetailsComponent>
-        </div>
+          <DetailsWrapper render = {getDetails1}></DetailsWrapper>
       )}
       {selectedPokemon2 && (
-        <div>
-          
-          <PokemonDetailsComponent2 pokemon = {selectedPokemon2}></PokemonDetailsComponent2>
-        </div>
+          <DetailsWrapper render = {getDetails2}></DetailsWrapper >
       )}
       <h2>Lista de Pokémons</h2>
       <PokemonListComponent selectedPokemon={setSelectedPokemon} selectedPokemon2 = {setSelectedPokemon2}></PokemonListComponent>
