63542323e116c80229396d81f9330aef59c2c835
fergr01 <fergr01bbx@gmail.com>
2024-12-18 12:19:04 +0100

    nopse

--- contador-jacobo/contador-jacobo/vite.config.js
+++ contador-jacobo/contador-jacobo/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build' // Directiorio de salida
+  },
+  base: '/'  // Asegura que las rutas sean absolutas
+});
