1cc892d219c18071f0bb8b4fe26d118ee67f3a6e
Fergr0 <fergr01bbx@gmail.com>
2025-03-04 11:10:19 +0100

    llorando

--- agendaReact/src/http-common.js
+++ agendaReact/src/http-common.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "http://agendita-fernando-env.eba-spsuusfm.us-east-1.elasticbeanstalk.com:8080/api/v1",
   headers: {
     "Content-type": "application/json"
   }
 });
 
 export const axios2 = axios.create({
-  baseURL: "http://localhost:8081/api/v2",
+  baseURL: "http://agendita-fernando-env.eba-spsuusfm.us-east-1.elasticbeanstalk.com:8081/api/v2",
   headers: {
     "Content-type": "application/json"
   }
